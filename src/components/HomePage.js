623e9b7ca2c6a0f8ced11d0cf908da74e942ad55
Marten Zand-Vakili <martenzv@hotmail.de>
2022-03-03 15:41:54 +0100

    HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react"
+import { NavLink } from "react-router-dom"
+import "./HomePage.css"
+
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="HomepageBackground">
     
     <h1>React Project Management</h1>
-  
+
+    <NavLink to="/signup">Register</NavLink>
+    
     
     </div>
   )
