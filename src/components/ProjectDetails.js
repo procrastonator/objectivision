a78e18981f498e6e32b55e455d3cb04c6ef2715a
Marten Zand-Vakili <martenzv@hotmail.de>
2022-03-05 13:23:11 +0100

    https://colorfabb.com/media/wysiwyg/nothing-to-see-here.gif

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -57,6 +57,7 @@ export default function ProjectDetails() {
         {console.log(projectDetails)}
         <hr />
         <p>Goals:</p>
+        <div></div>
         <p> {element.title}</p>
         <p> {element.description}</p>
         <Link to={`/projects/${element._id}/update`}>
