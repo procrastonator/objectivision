b765a2bd3d0774f57f2aac892e7397601486c63b
Bart Huiskamp <bartolameus.h@gmail.com>
2022-03-07 16:14:31 +0100

    I am soo tired :(

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -2,13 +2,12 @@ import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import { AuthContext } from '../context/auth.context';
-// import { GoalContext } from '../context/finishedGoal.context';
 import "./ProjectDetails.css"
 
 
 export default function ProjectDetails() {
 
-  // const { status, toggleStatus } = useContext(GoalContext) 
+
 
   const { projectId } = useParams();
   const [projectDetails, setProjectDetails] = useState({undefined})
@@ -18,44 +17,34 @@ export default function ProjectDetails() {
   const [isDoneArr, setIsDoneArr] = useState([]);
   const [inProgress, setInProgress] = useState([]);
 
-  // const checkProgressStatus = () => {
-  //   if (result.data.goals.isDone === "true") {
-      
-  //   }
-  // }   
-
-  // we need to figure out that the user see a colu with in progress goals and and is done colum.
-  // we would write a functon that will do a if statment that: 
-  
 
   useEffect(() => {
+   
     axios
       .get(
         `${process.env.REACT_APP_API_URL}/projects/${projectId}`,
         { headers: { Authorization: `Bearer ${storedToken}` } }
         )
       .then((result) => {
-        console.log(result.data)
-        setProjectDetails(result.data)
-        // console.log(isDoneArr)
-        // console.log(inProgress)
-        for ( let i = 0 ; i < result.data.goals.length; i++){
-          // console.log(typeof result.data.goals[i].isDone)
-        if (result.data.goals[i].isDone === true) {
-          setIsDoneArr(isDoneArr.push(result.data.goals[i]))
-        } else {
-          setInProgress(inProgress.push(result.data.goals[i]))
-        }
-      }
 
+        setProjectDetails(result.data)
+
+        console.log(inProgress)
+       
+       
+       let newfilter = result.data.goals
+       let newDoneArr = newfilter.filter(element => element.isDone === true )
+       setIsDoneArr(newDoneArr)
+
+
+ 
+       let newInProgressArr = newfilter.filter(element => element.isDone === false )
+       setInProgress(newInProgressArr)
+
+        console.log(newDoneArr)
       })
       .catch();
-  }, [projectId]);
-
-  
-
-  
-
+  }, []);
 
 
   return (
@@ -75,23 +64,34 @@ export default function ProjectDetails() {
     </Link>
 
 
-    { projectDetails._id ===  undefined ?
-     <h1>Loading....</h1> :
-     projectDetails.goals.map((element, index) => {
+     <h3> Is done </h3>
+     {isDoneArr.map((element, index) => {
       return (
         <div key={element._id}>
-        {/* {console.log(projectDetails)} */}
         <hr />
-
+       
         <p>Goals:</p>
         <div >
-        {/* <div className={`UpdateGoal ${status}`}> */}
         <p> {element.title}</p>
         <p> {element.description}</p>
-        {/* <button onClick={toggleStatus}> No text
-        {status === "inProgress" ? "Finished" : "Not finsihed"}
-      
-        </button> */}
+        </div>
+
+        </div>
+        )
+    })   }
+
+        <hr />
+<h3> In Progress </h3>
+  {inProgress.map((element, index) => {
+      return (
+        <div key={element._id}>
+     
+        <p>Goals:</p>
+        <div >
+        <p> {element.title}</p>
+        <p> {element.description}</p>
+    
+       
         </div>
   
         <Link to={`/projects/${element._id}/update`}>
@@ -102,8 +102,6 @@ export default function ProjectDetails() {
         </div>
         )
     })   }
-   
-  
     
     
    
