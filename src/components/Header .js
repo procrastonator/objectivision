1188a446f5dd59527a2993d75d7e95da5a935945
Bart Huiskamp <bartolameus.h@gmail.com>
2022-03-10 11:21:49 +0100

    bart work on header

--- src/components/Header .js
+++ src/components/Header .js
@@ -4,41 +4,40 @@ import { AuthContext } from "../context/auth.context";
 import house from "./assets/house.png";
 import "./Header.css";
 
+
 export default function Header() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   // console.log(user)
 
   return (
-    <section className="background">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light background">
+        <div className="container-fluid">
           <NavLink className="navbar-brand" to="/">
             <div>
-              <img src={house} alt="home Icon" width="39" height="35" />
-              <div>Home</div>
+              <img src={house} alt="home Icon" width="42" height="42" />
             </div>
           </NavLink>
 
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
+          <div className>
+            <ul className="">
               {isLoggedIn && (
                 <>
                   <NavLink
-                    className="nav-link active"
+                    className="btn btn-outline-warning"
                     aria-current="page"
                     to="/projects"
                   >
                     Projects
                   </NavLink>
                   <NavLink
-                    className="nav-link active"
+                    className="btn btn-outline-warning"
                     aria-current="page"
                     to="/projects/create"
                   >
                     New Project
                   </NavLink>
                   &nbsp;
-                  <button onClick={logOutUser} to="/">
+                  <button className="btn btn-outline-warning" onClick={logOutUser} to="/">
                     Logout
                   </button>
                 </>
@@ -73,6 +72,6 @@ export default function Header() {
           </div>
         </div>
       </nav>
-    </section>
+  
   );
 }
